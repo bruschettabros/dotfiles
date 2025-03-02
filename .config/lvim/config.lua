7f8201cbd950c9b1f4c66d050100e3093ba730cd
Shaun Collins <shaun.collins@evaluagent.com>
2025-03-02 13:04:03 +0000

    Added config.lua for LunarVim

---
+++ .config/lvim/config.lua
@@ -0,0 +1,20 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+vim.g.mapleader = ' '
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.encoding = 'utf-8'
+vim.opt.syntax = 'on'
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorcolumn = true
