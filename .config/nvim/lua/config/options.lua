135227f769021fdb0c350dc84c201a5d96067a06
Shaun Collins <shaun.collins@evaluagent.com>
2025-03-13 09:45:37 +0000

    Added cursor command for ideavim and added whichkey related stuff. There is also changes to tmux

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,11 @@
 --
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.tabstop = 4 -- Set tab width to 4 spaces
+vim.opt.shiftwidth = 4 -- Set indentation width to 4 spaces
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.syntax = "on" -- Enable syntax highlighting
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.smartindent = true -- Enable smart indentation
