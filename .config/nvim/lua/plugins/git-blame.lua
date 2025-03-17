bc1a7f11c5ce5903a70b7f83aeee525f63d02922
Shaun Collins <shaun.collins@evaluagent.com>
2025-03-17 13:39:52 +0000

    Removed commments from git blame

--- .config/nvim/lua/plugins/git-blame.lua
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -3,10 +3,10 @@ return {
   event = "VeryLazy",
   opts = {
     -- your configuration comes here
-    enabled = true, -- if you want to enable the plugin
-    -- message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-    message_template = "<author> • <date> • <<sha>>", -- template for the blame message, check the Message template section for more options
-    date_format = "%d-%m-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-    virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    enabled = true,
+    -- message_template = " <summary> • <date> • <author> • <<sha>>",
+    message_template = "<author> • <date> • (<summary>) ",
+    date_format = "%r",
+    virtual_text_column = 1,
   },
 }
