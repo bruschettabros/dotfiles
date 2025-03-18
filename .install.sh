9c7b8c1ffe78cb18a6466499d8433c79f44dfc46
Shaun Collins <shaun.collins@evaluagent.com>
2025-03-18 16:59:02 +0000

    Removed unneeded commands in install script

--- .install.sh
+++ .install.sh
@@ -3,15 +3,11 @@ OS=$(uname)
 #Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#Brew 
+#Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 #Brewfile
 brew bundle install --file=~/.dotfiles/Brewfile.$OSTYPE || echo "No Brewfile for $OSTYPE"
-
-#LazyVim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
 
 #Stow dotfiles
 cd ~/.dotfiles
