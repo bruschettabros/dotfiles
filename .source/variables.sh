40f0a4b407a17e16d5b14f0d3fa50efadff3fae1
Shaun Collins <shaun.collins@evaluagent.com>
2025-03-24 13:21:34 +0000

    added some more variables

--- .source/variables.sh
+++ .source/variables.sh
@@ -5,11 +5,14 @@ export MOTD=false
 
 # Work
 export WORK='evaluagent'
+export LNAME='collins'
+export FNAME='shaun'
 
 # Git
-export EMAIL="shaun.collins@$WORK.com"
+export EMAIL="$FNAME.$LNAME@$WORK.com"
 export GIT_COMMITTER_EMAIL=$EMAIL
 export GIT_AUTHOR_EMAIL=$EMAIL
+export GIT_COMMITTER_NAME="$FNAME $LNAME"
 
 # Editor
 export EDITOR='nvim'
